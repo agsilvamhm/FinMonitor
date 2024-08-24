5336bc42b4dbbbc695e5981c227df9517de468b8
Adalberto Gonçalves da Silva <agsilva.mhm@gmail.com>
2024-08-23 21:33:26 -0300

    Chamado do sobre funcionando

---
+++ app/src/main/java/com/agsilva/finmonitor/SobreActivity.java
@@ -0,0 +1,30 @@
+package com.agsilva.finmonitor;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SobreActivity extends AppCompatActivity {
+
+    public static void nova(AppCompatActivity activity){
+        Intent intent = new Intent(activity, SobreActivity.class);
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sobre);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
